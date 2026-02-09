6cfaa5f1db0c7e5e211b41a2da86f531cd28e394
Jake Wang <jakew1576@gmail.com>
2026-02-09 14:00:29 -0800

    Refactor Testing Class ProblemState into Node Class for A* Class Implementation

--- main.py
+++ main.py
@@ -1,81 +1,128 @@
-class ProblemSpace(object):
-    #FIXME: Goal state is hard-coded, make it dynamic to match the size of initial state later (currently expects a 3 by 3 2D array). Also add bad-input handling
-    def __init__(self, initialState):
-        self.currentState = initialState
-        self.rowMax = 2 #(highest array row index)
-        self.colMax = 2 #(highest array col index)
-        self.goalState = [[ 1,  2,  3 ], #Hardcoded, change later
-                          [ 4,  5,  6 ],
-                          [ 7,  8,  0 ]]
-    
-    #Returns where 0 is in currentState
-    def _emptySquareCoords(self):
-        for rowIndex, rowArr in enumerate(self.currentState):
-            for colIndex, colVal in enumerate(rowArr):
-                if colVal == 0:
-                    return rowIndex, colIndex
-    
-    #Swaps the values of squares 
-    def _swapSquares(self, rowIndex1, colIndex1, rowIndex2, colIndex2):
-        saveNum = self.currentState[rowIndex1][colIndex1]
-        self.currentState[rowIndex1][colIndex1] = self.currentState[rowIndex2][colIndex2]
-        self.currentState[rowIndex2][colIndex2] = saveNum
-    
-    def moveEmptySquareLeft(self):
-        rowIndex, colIndex = self._emptySquareCoords()
-        if colIndex == 0:
-            print("Cannot move empty square left.")
-            return False
-        
-        self._swapSquares(rowIndex, colIndex, rowIndex, colIndex-1)
-        return True
+import heapq
 
+# ----- Some Random Leftover Helper Functions that I Might Want to Use Later, Delete Later if not -----
 def display2DArray(arr):
     for row in arr:
         print(row)
 
-def distanceBetween(rowIndex1, colIndex1, rowIndex2, colIndex2):
+def distanceBetween(rowIndex1, colIndex1, rowIndex2, colIndex2): #Moved as a helper function in Node Class for Manhattan Distance calc function to use
     return abs(rowIndex1 - rowIndex2) + abs(colIndex1 - colIndex2)
 
-#FIXME: Hardcoded to 3 by 3, find a way to dynamically give provide goalState coords
-def getManhattanDistance(arr):
-    heuristic = 0
 
-    for rowIndex, rowArr in enumerate(arr):
-        for colIndex, colVal in enumerate(rowArr):
-            if( colVal == 0 ):
-                heuristic += distanceBetween(rowIndex, colIndex, 2, 2)
-            elif( colVal == 1 ):
-                heuristic += distanceBetween(rowIndex, colIndex, 0, 0)
-            elif( colVal == 2 ):
-                heuristic += distanceBetween(rowIndex, colIndex, 1, 0)
-            elif( colVal == 3 ):
-                heuristic += distanceBetween(rowIndex, colIndex, 2, 0)
-            elif( colVal == 4 ):
-                heuristic += distanceBetween(rowIndex, colIndex, 0, 1)
-            elif( colVal == 5 ):
-                heuristic += distanceBetween(rowIndex, colIndex, 1, 1)
-            elif( colVal == 6 ):
-                heuristic += distanceBetween(rowIndex, colIndex, 2, 1)
-            elif( colVal == 7 ):
-                heuristic += distanceBetween(rowIndex, colIndex, 0, 2)
-            elif( colVal == 8 ):
-                heuristic += distanceBetween(rowIndex, colIndex, 1, 2)
+# state is a 2D array: [[ 1,  2,  3 ],
+                    #   [ 4,  5,  6 ],
+                    #   [ 7,  8,  0 ]]
+# depth is how the number of moves needed to reach this node
+# fn = depth (gn) + heuristic (hn)
+# herusticType: 0 - Misplaced Tile, 1 - Manhattan Distance, Other - None
+class Node:
+    def __init__(self, state, depth, heuristicType):
+        self.state = state
+        self.depth = depth
+        self.heuristicType = heuristicType
+        self.fn = self._calculateFn(heuristicType)
 
-    return heuristic
+    def _getMisplacedTileCount(self, arr):
+        heuristic = (len(arr) * len(arr[0])) - 1 # (RowCount * ColCount) - 1 or Num tiles excluding 0
+        idealValue = 1
 
-#FIXME: Hardcoded to 3 by 3, find a way to dynamically give provide goalState coords
-def getMisplacedTileCount(arr):
-    heuristic = 0
-    i = 8
+        for rowIndex, rowArr in enumerate(arr):
+            for colIndex, colVal in enumerate(rowArr):
+                if( colVal != 0 and colVal == idealValue ):
+                    heuristic -= 1
+                idealValue += 1
+        return heuristic
+    
+    def _distanceBetween(self, rowIndex1, colIndex1, rowIndex2, colIndex2):
+        return abs(rowIndex1 - rowIndex2) + abs(colIndex1 - colIndex2)
 
-    for rowIndex, rowArr in enumerate(arr):
-        for colIndex, colVal in enumerate(rowArr):
-            if( colVal != 0 and colVal == i ):
-                heuristic += 1
-            i - 1
+    #FIXME: Hardcoded to 3 by 3, find a way to dynamically give provide goalState coords
+    def _getManhattanDistance(self,arr):
+        heuristic = 0
 
-    return heuristic
+        for rowIndex, rowArr in enumerate(arr):
+            for colIndex, colVal in enumerate(rowArr):
+                if( colVal == 1 ):
+                    heuristic += self._distanceBetween(rowIndex, colIndex, 0, 0)
+                elif( colVal == 2 ):
+                    heuristic += self._distanceBetween(rowIndex, colIndex, 1, 0)
+                elif( colVal == 3 ):
+                    heuristic += self._distanceBetween(rowIndex, colIndex, 2, 0)
+                elif( colVal == 4 ):
+                    heuristic += self._distanceBetween(rowIndex, colIndex, 0, 1)
+                elif( colVal == 5 ):
+                    heuristic += self._distanceBetween(rowIndex, colIndex, 1, 1)
+                elif( colVal == 6 ):
+                    heuristic += self._distanceBetween(rowIndex, colIndex, 2, 1)
+                elif( colVal == 7 ):
+                    heuristic += self._distanceBetween(rowIndex, colIndex, 0, 2)
+                elif( colVal == 8 ):
+                    heuristic += self._distanceBetween(rowIndex, colIndex, 1, 2)
+            return heuristic
+
+    def _calculateFn(self, heuristicType):
+        if heuristicType == 0:
+            return self.depth + self._getMisplacedTileCount(self.state)
+        elif heuristicType == 1:
+            return self.depth + self._getManhattanDistance(self.state)
+        else:
+            return self.depth
+
+    #Returns where 0 is in state
+    def _emptySquareCoords(self):
+        for rowIndex, rowArr in enumerate(self.state):
+            for colIndex, colVal in enumerate(rowArr):
+                if colVal == 0:
+                    return rowIndex, colIndex
+
+    #Helper function to check if a move in a chosen direction is allowed (0 - Up, 1 - Right, 2 - Down, 3 - Left)
+    def _tryDirection(self, direction):
+        zeroRowIndex, zeroColIndex = self._emptySquareCoords()
+        if direction == 0:
+            return zeroRowIndex != 0 #Can it go further Up from upmost row?
+        elif direction == 1:
+            return zeroColIndex != (len(self.state[zeroRowIndex])-1) #Can it go further Right from rightmost column?
+        elif direction == 2:
+            return zeroRowIndex != (len(self.state)-1) #Can it go further Down from downmost row?
+        elif direction == 3:
+            return zeroColIndex != 0 #Can it go further Left from leftmost column?
+        else:
+            raise ValueError("direction must be 0..3")
+        
+    #Swaps the values of squares 
+    def _swapSquares(self, arr, rowIndex1, colIndex1, rowIndex2, colIndex2):
+        saveNum = arr[rowIndex1][colIndex1]
+        arr[rowIndex1][colIndex1] = arr[rowIndex2][colIndex2]
+        arr[rowIndex2][colIndex2] = saveNum
+    
+    #Helper function to move empty square in a chosen direction (0 - Up, 1 - Right, 2 - Down, 3 - Left)
+    #ONLY USE AFTER HAVING CALLED _tryDirection
+    def _moveEmptySquare(self, arr, direction):
+        rowIndex, colIndex = self._emptySquareCoords()
+        if direction == 0: #Up
+            self._swapSquares(arr, rowIndex, colIndex, rowIndex+1, colIndex)
+        elif direction == 1: #Right
+            self._swapSquares(arr, rowIndex, colIndex, rowIndex, colIndex+1)
+        elif direction == 2: #Down
+            self._swapSquares(arr, rowIndex, colIndex, rowIndex-1, colIndex)
+        elif direction == 3: #Left
+            self._swapSquares(arr, rowIndex, colIndex, rowIndex, colIndex-1)
+        else:
+            raise ValueError("direction must be 0..3")
+
+    #Try moving node in each direction, return array of possible children
+    def generateChildren(self):
+        children = []
+        i = 0
+        #Generate child nodes of each possible state (max 4, min 2) that can derive from parent.
+        while i < 4:
+            if self._tryDirection(i):
+                newState = self._moveEmptySquare(self.state, i)
+                child = Node(child, newState, self.depth + 1, self.heuristicType)
+                children.append(child)
+            i += 1
+        
+        return children
 
 # ----- Main -----
 if __name__ == '__main__':
@@ -83,21 +130,3 @@ if __name__ == '__main__':
             [4, 6, 1],
             [3, 5, 8,]])
     
-    problemSpace = ProblemSpace(arr)
-    
-    print("Display arr and try to move it left (should fail)")
-    display2DArray(problemSpace.currentState)
-    problemSpace.moveEmptySquareLeft()
-
-    print("Swap the top left and bottom middle")
-    problemSpace._swapSquares(0,0,2,1)
-    display2DArray(problemSpace.currentState)
-
-    print("Print where the empty square is, move the empty square to the left, should swap 0 and 3")
-    row, col = problemSpace._emptySquareCoords()
-    print(f"Row: {row}, Col: {col}")
-    problemSpace.moveEmptySquareLeft()
-    display2DArray(problemSpace.currentState)
-
-    row, col = problemSpace._emptySquareCoords()
-    print(f"Row: {row}, Col: {col}")
